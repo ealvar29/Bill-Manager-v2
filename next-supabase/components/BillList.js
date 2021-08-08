4c542fcd671b4cd959be563e143bc56a11586279
Eduardo <ealvarez@carminaticonsulting.com>
2021-08-08 14:10:54 -0500

    moved bill list => Bill component

--- next-supabase/components/BillList.js
+++ next-supabase/components/BillList.js
@@ -6,8 +6,9 @@ import { Fragment } from "react";
 import { supabase } from "../lib/initSupabase";
 import { CheckIcon, SelectorIcon } from "@heroicons/react/solid";
 import Alert from "./Alert";
+import Bill from "./Bill";
 
-export default function Todos({ user }) {
+export default function BillList({ user }) {
   const [todo, setTodos] = useState([]);
   const [newTaskText, setNewTaskText] = useState("");
   const [errorText, setError] = useState("");
@@ -72,8 +73,8 @@ export default function Todos({ user }) {
 
   return (
     <div className="w-full">
-      <h1 className="mb-12">Todo List.</h1>
-      <div className="flex gap-2 my-2">
+      <h1 className="mb-12">Budget App v2.0</h1>
+      <div className="grid gap-2 my-2 border">
         <div className="mb-4">
           <Input
             className="w-full px-3 py-3 font-bold rounded text-grey-darker"
@@ -87,7 +88,7 @@ export default function Todos({ user }) {
         </div>
         <div className="mb-4">
           <InputNumber
-            className="w-full px-3 py-5 font-bold border rounded text-grey-darker"
+            className="w-full px-3 py-5 font-bold rounded text-grey-darker"
             label="Input Amount"
             max={1000000}
             value={cost}
@@ -116,74 +117,3 @@ export default function Todos({ user }) {
     </div>
   );
 }
-
-const Bill = ({ expense, onDelete }) => {
-  const [isCompleted, setIsCompleted] = useState(expense.is_complete);
-
-  const toggle = async () => {
-    try {
-      const { data, error } = await supabase
-        .from("expenses")
-        .update({ is_complete: !isCompleted })
-        .eq("id", expense.id)
-        .single();
-      if (error) {
-        throw new Error(error);
-      }
-      setIsCompleted(data.is_complete);
-    } catch (error) {
-      console.log("error", error);
-    }
-  };
-
-  return (
-    <li
-      onClick={(e) => {
-        e.preventDefault();
-        toggle();
-      }}
-      className="block w-full transition duration-150 ease-in-out cursor-pointer hover:bg-gray-200 focus:outline-none focus:bg-gray-200"
-    >
-      <div className="flex items-center px-4 py-4 sm:px-6">
-        <div className="flex items-center flex-1 min-w-0">
-          <div className="text-sm font-medium leading-5 truncate">
-            {expense.billname}
-          </div>
-        </div>
-        <div className="flex items-center flex-1 min-w-0">
-          <div className="text-sm font-medium leading-5 truncate">
-            {expense.cost}
-          </div>
-        </div>
-        <div>
-          <input
-            className="cursor-pointer"
-            onChange={(e) => toggle()}
-            type="checkbox"
-            checked={isCompleted ? true : ""}
-          />
-        </div>
-        <button
-          onClick={(e) => {
-            e.preventDefault();
-            e.stopPropagation();
-            onDelete();
-          }}
-          className="w-4 h-4 ml-2 border-2 rounded hover:border-black"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="gray"
-          >
-            <path
-              fillRule="evenodd"
-              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </button>
-      </div>
-    </li>
-  );
-};
